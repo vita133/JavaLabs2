b7195316221cb04dbb51efac3ccff389652464ee
vita133 <vitandrukhiv@gmail.com>
2023-10-11 21:34:44 +0300

    add transposeMatrix, findRowMaxSums and calculate methods

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         MatrixCalculator matrixCalculator = new MatrixCalculator(3, 3, -10.9F, 5.5F);
+        matrixCalculator.calculate();
     }
 }
--- src/main/java/org/example/MatrixCalculator.java
+++ src/main/java/org/example/MatrixCalculator.java
@@ -3,16 +3,18 @@ package org.example;
 import java.util.Random;
 
 public class MatrixCalculator {
-    private float[][] matrix;
+    private final float[][] matrix;
+    private final float[][] matrixTranspose;
 
     public MatrixCalculator(int rows, int cols, float minValue, float maxValue) {
         if (rows <= 0 || cols <= 0) {
             throw new IllegalArgumentException("Matrix dimensions must be positive");
         }
         matrix = generateRandomMatrix(rows, cols, minValue, maxValue);
+        matrixTranspose = new float[cols][rows];
     }
 
-    public static float[][] generateRandomMatrix(int rows, int cols, float minValue, float maxValue) {
+    private float[][] generateRandomMatrix(int rows, int cols, float minValue, float maxValue) {
         float[][] generatedMatrix = new float[rows][cols];
         Random random = new Random();
 
@@ -24,7 +26,41 @@ public class MatrixCalculator {
         return generatedMatrix;
     }
 
-    public void printMatrix() {
+    private void transposeMatrix() {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                matrixTranspose[i][j] = matrix[j][i];
+            }
+        }
+    }
+
+    private float findRowMaxSums() {
+        int rows = matrixTranspose.length;
+        int cols = matrixTranspose[0].length;
+        float result = 0.0F;
+
+        for (int i = 0; i < rows; i++) {
+            float max = Float.NEGATIVE_INFINITY;
+            for (int j = 0; j < cols; j++) {
+                if (matrixTranspose[i][j] > max) {
+                    max = matrixTranspose[i][j];
+                }
+            }
+            result += max;
+        }
+        return result;
+    }
+
+    public void calculate(){
+        printMatrix(matrix);
+        transposeMatrix();
+        printMatrix(matrixTranspose);
+        System.out.println(findRowMaxSums());
+    }
+    private void printMatrix(float[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
 
